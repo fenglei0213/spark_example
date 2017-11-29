3fa2de572e5e14cd907cecabfcef9c5e5860cda5
fenglei <fenglei@baidu.com>
2017-11-29 16:15:45 +0800

    add join

--- src/main/scala/org/sample/JoinCase.scala
+++ src/main/scala/org/sample/JoinCase.scala
@@ -9,20 +9,45 @@ object JoinCase {
     val aFilePath = "file:///code/github/spark_example/data/a.txt" // Should be some file on your system
     val bFilePath = "file:///code/github/spark_example/data/a.txt" // Should be some file on your system
     val conf = new SparkConf().setAppName("Join Application").setMaster("local");
+    //
+    val conf = new SparkConf().setAppName("Join Application").setMaster("local");
     val sc = new SparkContext(conf)
+//    val cDataRdd = aDataRdd.map(row=>row.split(";")).map(row=>{(row._1,(row._1,row._2))}).leftOuterJoin(bDataRdd)
+//    println(cDataRdd.foreach(println))
+
+    var rdd1 = sc.makeRDD(Array(("A","1"),("B","2"),("C","3")),2)
+    var rdd2 = sc.makeRDD(Array(("A","a"),("C","c"),("D","d")),2)
+    rdd1.leftOuterJoin(rdd2).collect
+
+
+    //
     val aDataRdd = sc.textFile(aFilePath, 2).cache()
+    val aDataRddNew = aDataRdd.map(line => {
+      val lineArray = line.split(";")
+      (lineArray(0), lineArray(1))
+    })
     val bDataRdd = sc.textFile(bFilePath, 2).cache()
-    println(bDataRdd.toString())
+    val bDataRddNew = bDataRdd.map(line => {
+      var lineArray = line.split(";")
+      (lineArray(0), lineArray(1))
+    })
+    val cDataRddNew = aDataRddNew.leftOuterJoin(bDataRddNew);
+    //    cDataRddNew.foreach(line => {
+    //      println(line)
+    //    });
+    println(cDataRddNew.count())
+    val outputFile = "file:///code/github/spark_example/data/output_rs"
+    cDataRddNew.saveAsTextFile(outputFile)
 
 //    val joinRdd = aDataRdd join bDataRdd
-    aDataRdd.foreach(f => println(f))
+//    aDataRdd.foreach(f => println(f))
 //    aDataRdd.foreach(f => if(bDataRdd.))
 
 //    val rdd0 = sc.parallelize((1,1),(1,2),(1,3),(2,1),(2,2),(2,3), 3)
 //    val rdd5 = rdd0.join(rdd0)
-//    aDataRdd.co
+
 //    aDataRdd.collect
-    val output = "C:/code/github/spark_example/data/wordcount-" + System.currentTimeMillis();
+//    val output = "C:/code/github/spark_example/data/wordcount-" + System.currentTimeMillis();
 //    joinRdd.saveAsTextFile(output) //save result to local file or HDFS
   }
 }
--- src/main/scala/org/sample/MapCase.scala
+++ src/main/scala/org/sample/MapCase.scala
@@ -1,4 +1,6 @@
-package org.wind3stone.spark.sample;
+package org.wind3stone.spark.sample
+
+;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
@@ -10,33 +12,47 @@ object MapCase {
     val conf = new SparkConf().setAppName("Join Application").setMaster("local");
     val sc = new SparkContext(conf)
     //
-        val aFilePath = "file:///code/github/spark_example/data/a.txt"
-        val aDataRdd = sc.textFile(aFilePath, 2).cache()
-        val aDataRddNew = aDataRdd.map(line =>
-          line.split(";"))
-        aDataRddNew.foreach(println);
+    val aFilePath = "file:///code/github/spark_example/data/a.txt"
+    val bFilePath = "file:///code/github/spark_example/data/b.txt"
+    val aDataRdd = sc.textFile(aFilePath, 2).cache()
+    val aDataRddNew = aDataRdd.map(line => {
+      val lineArray = line.split(";")
+      (lineArray(0), lineArray(1))
+    })
+    val bDataRdd = sc.textFile(bFilePath, 2).cache()
+    val bDataRddNew = bDataRdd.map(line => {
+      var lineArray = line.split(";")
+      (lineArray(0), lineArray(1))
+    })
+    val cDataRddNew = aDataRddNew.leftOuterJoin(bDataRddNew);
+//    cDataRddNew.foreach(line => {
+//      println(line)
+//    });
+    println(cDataRddNew.count())
+    val outputFile = "file:///code/github/spark_example/data/output_rs"
+    cDataRddNew.saveAsTextFile(outputFile)
 
-//    val mapRdd = sc.parallelize(List(1, 2, 3, 3))
-//    val mapOutRdd = mapRdd.map(x => x + 1)
-//    mapOutRdd.foreach(println)
+    //    val mapRdd = sc.parallelize(List(1, 2, 3, 3))
+    //    val mapOutRdd = mapRdd.map(x => x + 1)
+    //    mapOutRdd.foreach(println)
 
 
-//    val aDataRddNew = aDataRdd.map(line =>
-//      line + "_NEWS")
+    //    val aDataRddNew = aDataRdd.map(line =>
+    //      line + "_NEWS")
 
-//
-//    val flatMapOutRdd = mapRdd.flatMap(x => x.to(3))
-//    flatMapOutRdd.foreach(println)
+    //
+    //    val flatMapOutRdd = mapRdd.flatMap(x => x.to(3))
+    //    flatMapOutRdd.foreach(println)
 
-//    3.to(8).foreach {
-//      i => println(i)
-//    }
+    //    3.to(8).foreach {
+    //      i => println(i)
+    //    }
 
-//    val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", " eagle"), 2)
-//    val b = a.map(x => (x, 1))
-//    b.collect.foreach(println(_))
+    //    val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", " eagle"), 2)
+    //    val b = a.map(x => (x, 1))
+    //    b.collect.foreach(println(_))
     // 一样的
-//    b.collect.foreach(println)
+    //    b.collect.foreach(println)
 
   }
 
